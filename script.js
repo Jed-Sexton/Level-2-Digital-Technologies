fe1b8f03728df4b2939afd680e51028ea4edc198
Jed-Sexton <jedsexton10@gmail.com>
2026-07-13 19:44:46 +1200

    Added images and worked on popup
    Firstly I ensured the popup was working and formatted all correctly. I then redid the code to ensure that I only had one pop up and then there is a list in javascript which holds the subject name and equipment. Then I added all of the buttons for different subjects into the website and ensured all of the formatting was how I want it to be.

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
-function openPopup() {
+const equipment = {
+
+  Mathematics: [
+      "Scientific Calculator",
+      "Exercise Book",
+      "Pencil",
+      "Ruler"
+  ],
+
+  English: [
+      "Exercise Book",
+      "Blue Pen",
+      "Novel"
+  ],
+
+  Biology: [
+      "Exercise Book",
+      "Calculator",
+      "Pen"
+  ],
+
+  Physics: [
+      "Scientific Calculator",
+      "Exercise Book",
+      "Ruler"
+  ],
+
+  Chemistry: [
+      "Scientific Calculator",
+      "Lab Book",
+      "Pen"
+  ]
+
+};
+
+function openPopup(subject){
+
+  document.getElementById("subjectTitle").textContent = subject;
+
+  const list = document.getElementById("equipmentList");
+
+  list.innerHTML = "";
+
+  equipment[subject].forEach(function(item){
+
+      const li = document.createElement("li");
+
+      li.textContent = item;
+
+      list.appendChild(li);
+
+  });
+
   document.getElementById("popup").style.display = "flex";
+
 }
 
-function closePopup() {
+function closePopup(){
+
   document.getElementById("popup").style.display = "none";
+
 }
